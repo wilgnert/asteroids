96a98b11aa0a5d4a4e953e2ea7de9a4404b2a75e
wilgnert <wilgnert@hotmail.com>
2025-04-11 23:57:57 -0300

    limit framerate

--- game.py
+++ game.py
@@ -2,23 +2,34 @@ import pygame
 from constants import *
 
 class Game:
-  screen_width = SCREEN_WIDTH
-  screen_height = SCREEN_HEIGHT
-  screen = None
-  __is_running = False
+  __screen_width = SCREEN_WIDTH
+  __screen_height = SCREEN_HEIGHT
+  __screen: pygame.Surface = None
+  __is_running: bool = False
+  __clock: pygame.time.Clock = None
+  __dt = 0
+  __t = 0
 
   def run():
     if Game.__is_running:
       return
     Game.__is_running = True
     pygame.init()
-    Game.screen = pygame.display.set_mode((Game.screen_width, Game.screen_height))
+    Game.__screen = pygame.display.set_mode((Game.__screen_width, Game.__screen_height))
+    Game.__clock = pygame.time.Clock()
+    Game.__dt = 0
+    Game.__t = 0
+
+
+    Game.__loop()
+  
+  def __loop():
     while True:
       Game.__handle_events()
       Game.__input()
       Game.__update()
       Game.__render()
-  
+
   def __handle_events():
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
@@ -29,10 +40,14 @@ class Game:
     pass
 
   def __update():
-    pass
+    Game.__dt = Game.__clock.tick(60) / 1000.0
 
   def __render():
-    Game.screen.fill((0, 0, 0))  # Fill the screen with black
+    Game.__screen.fill((0, 0, 0))  # Fill the screen with black
+    font = pygame.font.Font(None, 36)
+    text = font.render(f"Deltatime: {int(Game.__dt * 1000)} ms", True, (255, 255, 255))
+    text_rect = text.get_rect(center=(Game.__screen_width // 2, Game.__screen_height // 2)) 
+    Game.__screen.blit(text, text_rect)  # Draw the text on the screen
     
 
     pygame.display.flip()  # Update the display
